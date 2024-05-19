457a7bc0f0bedadb199ebd11d7fb7811c5b4ea41
Wojdak <jakub.wojdak11@gmail.com>
2024-05-19 17:06:36 +0200

    Changed database to Azure DB and removed connection string

--- StudyPlannerAPI/Program.cs
+++ StudyPlannerAPI/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-                   options.UseSqlServer(builder.Configuration.GetConnectionString("TemporaryDatabase")));
+                   options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnString")));
 
 builder.Services.AddAutoMapper(typeof(Program));
 
