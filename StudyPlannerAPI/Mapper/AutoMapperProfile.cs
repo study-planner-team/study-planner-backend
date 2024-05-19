2478552a8394753f5b9b1bdb5d63cc7b9000e337
Wojdak <jakub.wojdak11@gmail.com>
2024-05-19 16:07:47 +0200

    SP-54 Created "/register" endpoint

---
+++ StudyPlannerAPI/Mapper/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using StudyPlannerAPI.Models.DTO;
+using StudyPlannerAPI.Models;
+using AutoMapper;
+
+namespace StudyPlannerAPI.Mapper
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<UserRegistrationDTO, User>()
+               .ForMember(dest => dest.PasswordHash, opt => opt.MapFrom(src => BCrypt.Net.BCrypt.HashPassword(src.Password)));
+        }
+    }
+}
---
+++ StudyPlannerAPI/Models/DTO/UserRegistrationDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace StudyPlannerAPI.Models.DTO
+{
+    public class UserRegistrationDTO
+    {
+        public required string Username { get; set; }
+
+        public required string Password { get; set; }
+        public required string Email { get; set; }
+    }
+}
