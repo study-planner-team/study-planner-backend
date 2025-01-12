62e821c97808e52ff6dbc2e153300c4bd84fea4f
Wojdak <jakub.wojdak11@gmail.com>
2025-01-12 11:20:48 +0100

    Added unit tests for BadgeService

---
+++ StudyPlanner/Common/EntityFactories/Badges/UserBadgeFactory.cs
@@ -0,0 +1,23 @@
+﻿using StudyPlannerAPI.Models.Badges;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyPlannerTests.Common.EntityFactories.Badges
+{
+    public static class UserBadgeFactory
+    {
+        public static UserBadge CreateUserBadge(int userBadgeId, int userId, int badgeId)
+        {
+            return new UserBadge
+            {
+                UserBadgeId = userBadgeId,
+                UserId = userId,
+                BadgeId = badgeId,
+                EarnedDate = DateTime.UtcNow
+            };
+        }
+    }
+}
